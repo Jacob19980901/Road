dd2113fad1f368194b941a47754a02a41dcdf07d
Javokhir <mrwrestlemaniajacob@gmail.com>
2021-09-11 10:21:08 +0500

    test

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const selectMap = (mapName) => {
-  const mapEl = document.getElementsByClassName(mapName)[0];
-  const allActive = document
-    .querySelectorAll(".regions")[0]
-    .querySelectorAll(".active");
-  allActive.forEach((el) => {
-    el.classList.remove("active");
-  });
-  mapEl.classList.add("active");
-};
+// const selectMap = (mapName) => {
+//   const mapEl = document.getElementsByClassName(mapName)[0];
+//   const allActive = document
+//     .querySelectorAll(".regions")[0]
+//     .querySelectorAll(".active");
+//   allActive.forEach((el) => {
+//     el.classList.remove("active");
+//   });
+//   mapEl.classList.add("active");
+// };
